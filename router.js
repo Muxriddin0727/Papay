8f65eb8b2c2dd57db119a46dd352fb1437eb4b9f
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-05-30 18:22:51 +0900

    feat:member controllerlarni qurish

--- router.js
+++ router.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const memberController = require('./controllers/memberController');
 
-router.get("/", function (req, res) {
-    res.send("home sahifadasiz");
-});
+//memberga dahldor routerlar
+router.get("/", memberController.home);
+router.post("/signup", memberController.signup);
+router.post("/login", memberController.login);
+router.get("/logout", memberController.logout);
 
+//boshqa routerlar
 router.get("/menu", (req, res) => {
-    res.send("Menu sahifasidasiz");
+    res.send("Menu sahifadasiz");
 });
 
 router.get("/community", (req,res) => {
-    res.send("Jamiyat sahifasidasiz");
+    res.send("Jamiyat sahifadasiz");
 });
 
 module.exports = router;
