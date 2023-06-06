1062c0ddbfc131aff0479966a1e01a654613b76b
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-06-06 19:04:02 +0900

    few changes

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -76,6 +76,18 @@ restaurantController.logout = (req, res) => {
   res.send("logout sahifasidasiz");
 };
 
+restaurantController.validateAuthRestaurant = (req, res, next) => {
+  if (req.session?.member?.mb_type === "RESTAURANT") {
+    req.member = req.session.member;
+    next();
+  } else {
+    res.json({
+      state: "fail",
+      message: "only authrnticated members restaurant type",
+    });
+  }
+};
+
 restaurantController.checkSessions = (req, res) => {
   if (req.session?.member) {
     res.json({ state: "succeed", data: req.session.member });
