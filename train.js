1b4ca13a195896b851b591b4fe4dda08c531f814
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-06-08 15:53:50 +0900

    feat: task F ni qilib qoshdim

---
+++ train.js
@@ -0,0 +1,9 @@
+//F-Task: Shunday function tuzing, unga string argument pass bolsin. 
+//Function ushbu agrumentdagi faqat digitlarni yangi stringda return qilsin!
+//Masalan: findDigits('ad5we34jkf89') return qilishi kerak bolgan qiymat '53489'
+
+function findDigits(string) {
+    let number = string.replace(/[^0-9]/g, '');
+    
+};
+console.log('828shah67xbxbs98' .replace(/[^0-9]/g, ''));
