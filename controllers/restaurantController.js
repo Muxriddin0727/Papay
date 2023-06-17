42f9372b3903a384e80005ff03a649f9300b4aa6
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-06-17 19:27:45 +0900

    feat: signup page business logic on admin

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,5 +1,7 @@
+const Definer = require("../lib/mistake");
 const Member = require("../models/Member");
 const Product = require("../models/Product");
+const assert = require("assert");
 
 
 let restaurantController = module.exports;
@@ -41,17 +43,21 @@ restaurantController.getSignupMyRestaurant = async (req, res) => {
 
 restaurantController.signupProcess = async (req, res) => {
   try {
-    console.log("POST: cont/signupProces");
-    const data = req.body,
-      member = new Member(),
-      new_member = await member.signupData(data);
+    console.log("POST: cont/signupProcess");
+    assert(req.file, Definer.general_err3);
 
-      //session
-      req.session.member = new_member;
-      res.redirect ("/resto/products/menu");
+    let new_member = req.body;
+    new_member.mb_type = "RESTAURANT";
+    new_member.mb_image = req.file.path;
 
+    const member = new Member();
+    const result = await member.signupData(new_member);
+    assert(req.file, Definer.general_err1);
+
+    req.session.member = result;
+    res.redirect("/resto/products/menu");
   } catch (err) {
-    console.log(`ERROR, cont/signupProcess`);
+    console.log(`ERROR, cont/sign-up, ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
 };
