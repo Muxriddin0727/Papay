e09fc0596a202f7f0560c9b556add3d06cd5e88d
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-06-20 18:25:42 +0900

    feat:new product creation logic

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -27,7 +27,7 @@ restaurantController.getMyRestaurantProducts = async (req, res) => {
   
   } catch (err) {
     console.log(`ERROR, cont/getMyRestaurantProducts`);
-    res.json({ state: "fail", message: err.message });
+res.redirect("/resto");
   }
 };
 
